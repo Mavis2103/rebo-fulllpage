2d2faf03f750f7fedd86e7e01445cb80e0cb79f1
Mavis2103 <taquanvku@gmail.com>
2020-09-09 23:36:26 +0700

    update dashboard

--- app.js
+++ app.js
@@ -23,10 +23,7 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
-app.use(express.static(path.join(__dirname, '/node_modules/chart.js/dist')));
-app.use(express.static(path.join(__dirname, '/node_modules/jquery/dist')));
-app.use(express.static(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free')));
+app.use(express.static(path.join(__dirname, '/node_modules')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter);
--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,93 +1,93 @@
-var time_spending = document.getElementById('chart-time_spending').getContext('2d');
-var chart = new Chart(time_spending, {
-	// The type of chart we want to create
-	type: 'bar',
+// var time_spending = document.getElementById('chart-time_spending').getContext('2d');
+// var chart = new Chart(time_spending, {
+// 	// The type of chart we want to create
+// 	type: 'bar',
 
-	// The data for our dataset
-	data: {
-		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-		datasets: [
-			{
-				label: 'My First dataset',
-				backgroundColor: '#74b9ff',
-				borderColor: 'rgba(0, 0, 0, 0.1)',
-				data: [0, 10, 5, 2, 20, 30, 45],
-			},
-		],
-	},
+// 	// The data for our dataset
+// 	data: {
+// 		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+// 		datasets: [
+// 			{
+// 				label: 'My First dataset',
+// 				backgroundColor: '#74b9ff',
+// 				borderColor: 'rgba(0, 0, 0, 0.1)',
+// 				data: [0, 10, 5, 2, 20, 30, 45],
+// 			},
+// 		],
+// 	},
 
-	// Configuration options go here
-	options: {
-		scales: {
-			xAxes: [
-				{
-					gridLines: {
-						display: false,
-					},
-				},
-			],
-			yAxes: [
-				{
-					gridLines: {
-						display: false,
-					},
-					ticks: {
-						display: false,
-					},
-				},
-			],
-		},
-	},
-});
+// 	// Configuration options go here
+// 	options: {
+// 		scales: {
+// 			xAxes: [
+// 				{
+// 					gridLines: {
+// 						display: false,
+// 					},
+// 				},
+// 			],
+// 			yAxes: [
+// 				{
+// 					gridLines: {
+// 						display: false,
+// 					},
+// 					ticks: {
+// 						display: false,
+// 					},
+// 				},
+// 			],
+// 		},
+// 	},
+// });
 
-var growth = document.getElementById('chart-growth').getContext('2d');
-var chart = new Chart(growth, {
-	// The type of chart we want to create
-	type: 'line',
+// var growth = document.getElementById('chart-growth').getContext('2d');
+// var chart = new Chart(growth, {
+// 	// The type of chart we want to create
+// 	type: 'line',
 
-	// The data for our dataset
-	data: {
-		labels: ['', '', '', '', '', '', ''],
-		datasets: [
-			{
-				label: 'My First dataset',
-				backgroundColor: 'rgba(0,0,0,0)',
-				borderColor: '#e67e22',
-				data: [0, 10, 5, 2, 20, 30, 45],
-			},
-		],
-	},
+// 	// The data for our dataset
+// 	data: {
+// 		labels: ['', '', '', '', '', '', ''],
+// 		datasets: [
+// 			{
+// 				label: 'My First dataset',
+// 				backgroundColor: 'rgba(0,0,0,0)',
+// 				borderColor: '#e67e22',
+// 				data: [0, 10, 5, 2, 20, 30, 45],
+// 			},
+// 		],
+// 	},
 
-	// Configuration options go here
-	options: {
-		scales: {
-			xAxes: [
-				{
-					gridLines: {
-						display: false,
-					},
-				},
-			],
-			yAxes: [
-				{
-					gridLines: {
-						display: false,
-					},
-					ticks: {
-						display: false,
-					},
-				},
-			],
-		},
-		legend: {
-			display: false,
-		},
-		tooltips: {
-			callbacks: {
-				label: function (tooltipItem) {
-					return tooltipItem.yLabel;
-				},
-			},
-		},
-	},
-});
+// 	// Configuration options go here
+// 	options: {
+// 		scales: {
+// 			xAxes: [
+// 				{
+// 					gridLines: {
+// 						display: false,
+// 					},
+// 				},
+// 			],
+// 			yAxes: [
+// 				{
+// 					gridLines: {
+// 						display: false,
+// 					},
+// 					ticks: {
+// 						display: false,
+// 					},
+// 				},
+// 			],
+// 		},
+// 		legend: {
+// 			display: false,
+// 		},
+// 		tooltips: {
+// 			callbacks: {
+// 				label: function (tooltipItem) {
+// 					return tooltipItem.yLabel;
+// 				},
+// 			},
+// 		},
+// 	},
+// });
