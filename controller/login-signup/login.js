142857cce555587931c699ed4f5d34c0bdfebb2d
Mavis2103 <taquanvku@gmail.com>
2020-10-11 09:26:51 +0700

    fix

--- controller/login-signup/login.js
+++ controller/login-signup/login.js
@@ -7,19 +7,24 @@ const login = (req, res, next) => {
     let getUser = "select*from Account where email =?";
     db.query(getUser, [email], (error, data, field) => {
         if (error) return next(err);
-        bcrypt.compare(passwordI, data[0].password, (err, result) => {
-            if (result) {
-                req.session.userID = Buffer.from((data[0].userID), 'hex').toString('utf8');
-                req.session.username = data[0].username;
-                req.session.role = data[0].role;
-                req.session.avatar = data[0].avatar;
-                if (data[0].role == 'admin') {
-                    res.redirect("/users_management")
-                } else {
-                    res.redirect("/dashboard")
-                }
-            } else res.redirect("/login-signup")
-        })
+        if (data.length>0){
+            bcrypt.compare(passwordI, data[0].password, (err, result) => {
+                if (result) {
+                    req.session.userID = Buffer.from((data[0].userID), 'hex').toString('utf8');
+                    req.session.username = data[0].username;
+                    req.session.role = data[0].role;
+                    req.session.avatar = data[0].avatar;
+                    if (data[0].role == 'admin') {
+                        res.redirect("/users_management")
+                    } else {
+                        res.redirect("/dashboard")
+                    }
+                } else res.redirect("/login-signup")
+            })
+        }
+        else {
+            res.redirect('/login-signup')
+        }
     })
 }
 module.exports = login
