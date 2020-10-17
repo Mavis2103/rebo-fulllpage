4efb3f3afe05dd56af6816c588576b4fc9abfc5f
Mavis2103 <taquanvku@gmail.com>
2020-10-17 16:01:06 +0700

    update menu

--- public/javascripts/event.js
+++ public/javascripts/event.js
@@ -25,6 +25,33 @@ const dat = function date() {
   month.textContent = date.getMonth() + 1; // 0-11
   year.textContent = date.getFullYear();
 };
-
+const li_active = document.querySelectorAll('.menu li');
+window.addEventListener('load', () => {
+  switch (window.location.href.split('/')[3]) {
+    case 'dashboard':
+      li_active[0].classList.add('li--active');
+      break;
+    case 'library':
+      li_active[1].classList.add('li--active');
+      break;
+    case 'lesson':
+      li_active[2].classList.add('li--active');
+      break;
+    case 'tools':
+      li_active[3].classList.add('li--active');
+      break;
+    case 'myLibrary':
+      li_active[4].classList.add('li--active');
+      break;
+    case 'profile':
+      li_active[5].classList.add('li--active');
+      break;
+    case 'gift':
+      li_active[6].classList.add('li--active');
+      break;
+    default:
+      break;
+  }
+});
 // function Dates() {
 setTimeout(setInterval(dat, 1000), dat());
