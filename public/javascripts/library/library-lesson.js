d2c1e2cb25c8219614d7af970583c027544b9878
Mavis2103 <taquanvku@gmail.com>
2020-11-02 13:56:47 +0700

    fix

--- public/javascripts/library/library-lesson.js
+++ public/javascripts/library/library-lesson.js
@@ -1,17 +1,37 @@
+const img = document.getElementById('lesson-img');
+const name = document.getElementById('lesson-name');
+const auth = document.getElementById('lesson-auth');
+const bg_override = document.getElementById('bg-blur');
+const box_override = document.getElementById('box');
 const click_s = document.querySelectorAll('#s .swiper-slide');
 const get_href = document.querySelectorAll('#s .swiper-slide a');
 for (let i = 0; click_s.length; i += 1) {
 	click_s[i].addEventListener('click', (e) => {
 		e.preventDefault();
 		const x = get_href[i].getAttribute('href');
-		Get_Lesson(x);
+		bg_override.classList.remove('d-none');
+		box_override.classList.remove('d-none');
+		Get_Lesson(x).then((x) => {
+			name.textContent = x.lessonName;
+			auth.textContent = x.username;
+			img.src = `https://res.cloudinary.com/mavis/image/upload/Database_REBO/lessonImage/${x.lessonID}`;
+		});
+		document.getElementById('close').addEventListener('click', () => {
+			bg_override.classList.add('d-none');
+			box_override.classList.add('d-none');
+		});
 	});
 }
 async function Get_Lesson(x) {
 	const url = `/api/lesson/${x}`;
-	const response = await fetch(url, {
-		method: 'GET',
-	});
-	const result = await response.json();
-	console.log(result[0]);
+	try {
+		const response = await fetch(url, {
+			method: 'GET',
+		});
+		const result = await response.json();
+		console.log(result[0]);
+		return result[0];
+	} catch (error) {
+		console.log(error);
+	}
 }
