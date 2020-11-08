10aa1ad61c72a1abc3ad1782865497fb159f8214
Mavis2103 <taquanvku@gmail.com>
2020-11-08 17:20:49 +0700

    update library and menu responsive

--- controller/library/library.js
+++ controller/library/library.js
@@ -1,10 +1,14 @@
 const db = require('../../config/mysql');
 
 const show = (req, res, next) => {
-	const get = 'select Lesson.lessonName,Lesson.lessonID,Lesson.lessonDescription,Lesson.userID,Account.username from ((Lesson inner join Category on Category.categoryID= Lesson.categoryID) inner join Account on Account.userID=Lesson.userID)';
-	db.query(get, (err, data) => {
+	const get = 'select Lesson.lessonName,Lesson.lessonID,Lesson.lessonDescription,Lesson.userID,Account.username,Lesson.categoryID from ((Lesson inner join Category on Category.categoryID= Lesson.categoryID) inner join Account on Account.userID=Lesson.userID)';
+	const category = 'select*from Category';
+	db.query(`${get};${category}`, (err, data) => {
 		if (err) return next(err);
-		res.render('students/library/library', { lesson: data });
+		res.render('students/library/library', {
+			lessons: data[0],
+			categories: data[1],
+		});
 	});
 };
 
