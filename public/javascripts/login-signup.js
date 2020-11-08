d32324c29711ad041b3d946993784bd03f2f37a7
Mavis2103 <taquanvku@gmail.com>
2020-11-08 19:11:37 +0700

    fix page login-signup

--- public/javascripts/login-signup.js
+++ public/javascripts/login-signup.js
@@ -8,44 +8,44 @@ const title = document.querySelector('.content h1');
 const login = document.querySelector('.login');
 const signup = document.querySelector('.signup');
 document.getElementById('switch').addEventListener('change', () => {
-  if (elementSwitch.checked === false) {
-    signupON();
-  } else {
-    loginON();
-  }
+	if (elementSwitch.checked === false) {
+		signupON();
+	} else {
+		loginON();
+	}
 });
-// window.addEventListener("load", function () {
-//     if (elementSwitch.checked == false) {
-//         signupON()
-//     } else {
-//         loginON()
-//     }
-// })
+window.addEventListener('load', function () {
+	if (elementSwitch.checked == false) {
+		signupON();
+	} else {
+		loginON();
+	}
+});
 
 function loginON() {
-  bgEffect.classList.add('login--on');
-  title.textContent = 'Xin Chào bạn';
-  content.textContent = 'Nếu bạn chưa có tài khoản vui lòng đăng ký để tham gia với chúng tôi';
-  nameBtn.textContent = 'Đăng ký';
-  bgEffect.classList.remove('signup--on');
-  // Mobile
-  signup.classList.add('d-none');
-  login.classList.remove('d-none');
+	bgEffect.classList.add('login--on');
+	title.textContent = 'Xin Chào bạn';
+	content.textContent = 'Nếu bạn chưa có tài khoản vui lòng đăng ký để tham gia với chúng tôi';
+	nameBtn.textContent = 'Đăng ký';
+	bgEffect.classList.remove('signup--on');
+	// Mobile
+	signup.classList.add('d-none');
+	login.classList.remove('d-none');
 }
 
 function signupON() {
-  bgEffect.classList.add('signup--on');
-  title.textContent = 'Chào mừng trở lại';
-  content.textContent = 'Để giữ liên kết với chúng tôi vui lòng đăng nhập với thông tin của bạn';
-  nameBtn.textContent = 'Đăng nhập';
-  bgEffect.classList.remove('login--on');
-  // Mobile
-  login.classList.add('d-none');
-  signup.classList.remove('d-none');
+	bgEffect.classList.add('signup--on');
+	title.textContent = 'Chào mừng trở lại';
+	content.textContent = 'Để giữ liên kết với chúng tôi vui lòng đăng nhập với thông tin của bạn';
+	nameBtn.textContent = 'Đăng nhập';
+	bgEffect.classList.remove('login--on');
+	// Mobile
+	login.classList.add('d-none');
+	signup.classList.remove('d-none');
 }
 const imgUP = {
-  duration: 1000,
-  // reset: true
+	duration: 1000,
+	// reset: true
 };
 
 ScrollReveal().reveal('img', imgUP);
